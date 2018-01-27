0c660b14c097a4578c879fe3d7930b528a90826a
Miguel Rios <miguelriosoliveira@gmail.com>
2018-01-27 21:39:18 -0200

    Using free Bootstrap one-page no-scroll template designed by David Miller (http://davidmiller.io/)

---
+++ js/coming-soon.js
@@ -0,0 +1,13 @@
+(function ($) {
+	"use strict"; // Start of use strict
+
+	// Vide - Video Background Settings
+	$('body').vide({
+		// mp4: "mp4/bg.mp4",
+		// poster: "img/bg-mobile-fallback.jpg"
+		poster: "img/cantamires.jpg"
+	}, {
+		posterType: 'jpg'
+	});
+
+})(jQuery); // End of use strict
